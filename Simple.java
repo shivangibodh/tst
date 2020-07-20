d3909e92ce7a75efb1128f2d1897b7dca05799fe
shivangibodh <47949043+shivangibodh@users.noreply.github.com>
2020-07-20 14:25:27 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,40 @@
+class Simple{
+
+ 
+ 
+
+ 
+ 
+
+ 
+
+public static void main(String args[]){
+
+ 
+ 
+
+ 
+ 
+
+ 
+
+System.out.println("Hello Java");
+
+ 
+ 
+
+ 
+ 
+
+ 
+}
+
+ 
+ 
+
+ 
+
+ 
+
+ 
+}
